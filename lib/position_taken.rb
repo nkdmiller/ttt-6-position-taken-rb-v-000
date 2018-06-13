08a7490ad59fb1a736e0e242481e7b9e2827386f
Nate Miller <natekmiller96@gmail.com>
2018-06-13 16:21:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,12 @@
 def position_taken?(board,index)
   if board[index] == "X"||"O"
     return true
+    puts "true"
   elsif board[index] == " "||""||nil
     return false
+    puts "false"
   end
 end
+board == ["X"]
+index == 0
+position_taken?(board,index)
